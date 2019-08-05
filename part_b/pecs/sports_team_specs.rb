9bb6e60940e68dfa8c836f8b353a3c94d45be10e
Liliana Everett <everettliliana@gmail.com>
2019-08-05 19:53:18 +0100

    Part B, get 1 player

--- part_b/pecs/sports_team_specs.rb
+++ part_b/pecs/sports_team_specs.rb
@@ -21,11 +21,26 @@ class TestSportsTeam < MiniTest::Test
     assert_equal('Bruno Nascimento', sports_team.coach())
   end
 
-def test_set_new_coach
-  sports_team = SportsTeam.new('Benfica', ['Rafa', 'Raul', 'Pizzi', 'Odisseias'], 'Bruno Nascimento')
-  sports_team.new_coach('Rui Vitoria')
-  assert_equal('Rui Vitoria', sports_team.coach())
-end
+  def test_set_new_coach
+    sports_team = SportsTeam.new('Benfica', ['Rafa', 'Raul', 'Pizzi', 'Odisseias'], 'Bruno Nascimento')
+    sports_team.new_coach('Rui Vitoria')
+    assert_equal('Rui Vitoria', sports_team.coach())
+  end
+
+  def test_set_new_player
+    sports_team = SportsTeam.new('Benfica', ['Rafa', 'Raul', 'Pizzi', 'Odisseias'], 'Bruno Nascimento')
+    sports_team.new_player('Chiquinho')
+    assert_equal(['Rafa', 'Raul', 'Pizzi', 'Odisseias', 'Chiquinho'], sports_team.players())
+  end
+
+  # Add a method that takes in a string of a player's name and checks to see if they are in the players array.
+
+  def test_team_player_in_team
+     sports_team = SportsTeam.new('Benfica', ['Rafa', 'Raul', 'Pizzi', 'Odisseias'], 'Bruno Nascimento')
+     assert_equal('Pizzi', sports_team.players.at(2))
+  end
+
+
 
 
 end
--- part_b/sports_team.rb
+++ part_b/sports_team.rb
@@ -4,16 +4,24 @@ attr_accessor :name, :players, :coach
 
 # Team name (String), Players (array of strings) and a Coach (String).
 
-def initialize(input_name, input_players, input_coach)
+  def initialize(input_name, input_players, input_coach)
     # define our properties:
     @name = input_name
     @players = input_players
     @coach = input_coach
-end
+  end
 
-def new_coach(coach_name)
-  @coach = 'Rui Vitoria'
-end
+  def new_coach(coach_name)
+    @coach = 'Rui Vitoria'
+  end
+
+  def new_player(new_player_name)
+    @players.push(new_player_name)
+  end
+
+
+
+
 
 
 end
